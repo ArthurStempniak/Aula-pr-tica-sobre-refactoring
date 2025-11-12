8cc152e95dee5d73fbb3a567a33885321e5bff06
ArthurStempniak <arthurstempniak8@gmail.com>
2025-11-12 11:44:44 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -18,7 +18,6 @@ public class Customer {
    }
   
   public String statement() {
-
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
@@ -32,6 +31,25 @@ public class Customer {
      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
+   }
+
+
+   public String htmlStatement() {
+      Enumeration rentals = _rentals.elements();
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+
+         result += each.getMovie().getTitle()+ ": " +
+                   String.valueOf(each.getCharge()) + "<BR>\n";
+      }
+      
+
+      result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+             String.valueOf(getTotalFrequentRenterPoints()) +
+             "</EM> frequent renter points<P>";
+      return result;
    }
      
    private double getTotalCharge() {
